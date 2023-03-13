dd9cf68be5cef39b4003143d582ff331453bde04
Gabriel <gabrielgcr45@gmail.com>
2023-03-12 21:47:20 -0300

    bob

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,10 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let mut reversed = "".to_string();
-
-    input
-        .chars()
-        .rev()
-        .for_each(|c| reversed.push_str(&c.to_string()));
-
-    reversed
+    input.chars().rev().collect()
 }
